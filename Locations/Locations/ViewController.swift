f67d91a1c7b6976354ec2262911cddedd76c935b
Boris Chirino <bolek1976@yahoo.com>
2019-09-05 00:14:20 +0200

    Initial Commit

---
+++ Locations/Locations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Locations
+//
+//  Created by Boris Chirino on 05/09/2019.
+//  Copyright © 2019 Home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
